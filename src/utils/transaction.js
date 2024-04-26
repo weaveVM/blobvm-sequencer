a32211faaf27154a977070e6deae5ee6428a39f5
charmful0x <77340894+charmful0x@users.noreply.github.com>
2024-04-26 11:33:56 +0200

    feat: support  in context

--- src/utils/transaction.js
+++ src/utils/transaction.js
@@ -31,7 +31,7 @@ export async function evaluateTx(txid) {
 
     const normalizedSourceCode = normalizedString.split("").join("");
 
-    const code = `const msg = {}; msg.sender = "${from}"; `.concat(
+    const code = `const msg = {}; msg.sender = "${from}"; const tx = {}; tx.id = "${txid}"; const blobvm = {msg, tx}; `.concat(
       normalizedSourceCode,
     );
     const state = contract.LatestState;
